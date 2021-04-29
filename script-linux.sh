2baf1858e23e8898d38cacfc40902dfcb70b8d10
Chad Boyce <chadboyce@gmail.com>
2021-04-29 01:08:13 -0400

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -2,7 +2,11 @@ echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linu
 
 echo starting tmate config...
 
-tmate -k tmk-$TMATE_API -n testname
+# tmate -k tmk-$TMATE_API -n testname
+
+touch ~/.tmate.conf
+echo set tmate-api-key "$TMATE_API" >> ~/.tmate.conf
+echo set tmate-session-name "testname" >> ~/.tmate.conf
 
 echo tmate config set
 echo tmate name = testname 
